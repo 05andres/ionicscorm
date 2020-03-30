b89d152d5f28e7130fdb4b821f5ffa16875c14ca
carlos andres <carlos24lg@gmail.com>
2020-03-30 10:52:31 -0500

    scorm ionic

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { SecondPage } from '../modals/second/second.page';
+
+import { HomePage } from './home.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage
+      }
+    ])
+  ],
+  declarations: [HomePage,SecondPage],
+  entryComponents: [SecondPage]
+
+})
+export class HomePageModule {}
